450ca3948639231ab827c69d7daa480621945765
ltellez <ltellez.2009@gmail.com>
2019-08-29 17:06:27 -0500

    Input, Output,Viewchild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,30 +1,41 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-
+// RUTAS
+import { PAGES_ROUTES } from './pages.routes';
+// MODULOS PERSONALIZADOS
+import { SharedModule } from '../shared/shared.module';
+// COMPONENTES
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
-import { PAGES_ROUTES } from './pages.routes';
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
+// ng2 - Charts
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     PagesComponent,
     DashboardComponent,
     ProgressComponent,
-    Graficas1Component],
+    Graficas1Component,
+    IncrementadorComponent,
+    GraficoDonaComponent],
   imports: [
     SharedModule,
-    PAGES_ROUTES  
+    FormsModule,
+    ChartsModule,
+    PAGES_ROUTES
   ],
-  exports: [ 
+  exports: [
     DashboardComponent,
     ProgressComponent,
-    Graficas1Component,
-    
+    Graficas1Component
+
   ],
   providers: [],
 })
-export class PageModule {}
+export class PageModule { }
